57593dd44b80e88c2eee9bd7bcd10e7ac294e29f
Julio <mascittija@github.com>
2018-02-16 11:45:21 -0300

    3 finalizado

--- languagemodeling/ngram_generator.py
+++ languagemodeling/ngram_generator.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-import random
+from random import random
 
 
 class NGramGenerator(object):
@@ -8,31 +8,26 @@ class NGramGenerator(object):
         """
         model -- n-gram model.
         """
-        self._n = model._n
+        self._n = n = model._n
+        count = model._count
 
         # compute the probabilities
         probs = defaultdict(dict)
         # WORK HERE!!
         for key in list(count.keys()):
-            prev_tokens = key[:-1]
-            token = key[-1:]
-        #    print(prev_tokens)
-        #    print(key)
-        #    print(count.get(key))
-        #    print(count.get(prev_tokens))
             if len(key) == n:
-                probs[prev_tokens][token] = count.get(key)/count.get(prev_tokens)
-
+                prev_tokens = key[:-1]
+                token = key[n-1]
+                probs[prev_tokens][token] = model.cond_prob(token, prev_tokens)
+                
         self._probs = dict(probs)
 
         # sort in descending order for efficient sampling
         self._sorted_probs = sorted_probs = defaultdict(list)
         # WORK HERE!!
         for key in probs.keys():
-
             l = list(probs.get(key).items())
             l.sort(key=lambda x: x[1], reverse=True)
-        #    print(l)
             sorted_probs[key] = l
 
     def generate_sent(self):
@@ -44,9 +39,24 @@ class NGramGenerator(object):
         token = self.generate_token(tuple(prev_tokens))
         while token != '</s>':
             # WORK HERE!!
-            print('asd')
+            sent += list((token,))
+            prev_tokens += list((token,))
+            prev_tokens = prev_tokens[1:]
+            token = self.generate_token(tuple(prev_tokens))
 
         return sent
+
+    def sample(self,problist):
+        r = random()  # entre 0 y 1
+        i = 0
+        word, prob = problist[0]
+        acum = prob
+        while r > acum:
+            i += 1
+            word, prob = problist[i]
+            acum += prob
+        
+        return word
 
     def generate_token(self, prev_tokens=None):
         """Randomly generate a token, given prev_tokens.
@@ -58,8 +68,9 @@ class NGramGenerator(object):
             prev_tokens = ()
         assert len(prev_tokens) == n - 1
 
-        r = random.random()
+        r = random()
         probs = self._sorted_probs[prev_tokens]
         # WORK HERE!!
+        token = self.sample(probs)
 
         return token
